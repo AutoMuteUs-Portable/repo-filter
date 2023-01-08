6e1b8094c8fb14a8de58d54049ce298d1e369b2a
mtaku3 <m.taku3.1222@gmail.com>
2023-01-08 17:21:45 +0900

    doc: add docstring to commands

--- main.py
+++ main.py
@@ -20,12 +20,31 @@ def automuteus():
 
 
 @automuteus.command("init")
-@click.option("--input", type=click.Path(dir_okay=False, exists=True))
-@click.option("--output", type=click.Path(dir_okay=False, exists=False), required=True)
-@click.option("--force", is_flag=True, default=False)
-@click.argument("destination", type=click.Path(file_okay=False, exists=False))
+@click.option(
+    "--input",
+    type=click.Path(dir_okay=False, exists=True),
+    help="Path to dill file to be inputted",
+)
+@click.option(
+    "--output",
+    type=click.Path(dir_okay=False, exists=False),
+    required=True,
+    help="Path to dill file to be outputted",
+)
+@click.option(
+    "--force",
+    is_flag=True,
+    default=False,
+    help="Run git-filter-repo with --force option",
+)
 @click.argument(
-    "upstream-url", type=str, default="https://github.com/automuteus/automuteus"
+    "destination",
+    type=click.Path(file_okay=False, exists=False),
+)
+@click.argument(
+    "upstream-url",
+    type=str,
+    default="https://github.com/automuteus/automuteus",
 )
 def automuteus_init(
     input: Optional[str],
@@ -34,16 +53,52 @@ def automuteus_init(
     destination: str,
     upstream_url: str,
 ):
+    """
+    \b
+    Initialize automuteus repository for AutoMuteUs-Portable.
+
+    \b
+    Applying filter is necessary because the upstream repository is not designed to be built for Windows.
+    It makes possible to build Windows executable using Github Actions.
+
+    \b
+    After initializing repository, push to your own remote repository and push tags to run Github Actions.
+    If upstream reposiory is updated, run update command to apply filter to the new commits.
+
+    \b
+    Arguments:
+        destination: Path to destination(local repository) directory
+        upstream_url: URL to upstream repository
+    """
     Init(destination, input, output, upstream_url, force)
 
 
 @automuteus.command("update")
-@click.option("--input", type=click.Path(dir_okay=False, exists=True), required=True)
-@click.option("--output", type=click.Path(dir_okay=False, exists=False))
-@click.option("--force", is_flag=True, default=False)
-@click.argument("destination", type=click.Path(file_okay=False, exists=False))
+@click.option(
+    "--input",
+    type=click.Path(dir_okay=False, exists=True),
+    required=True,
+    help="Path to dill file to be inputted",
+)
+@click.option(
+    "--output",
+    type=click.Path(dir_okay=False, exists=False),
+    help="Path to dill file to be outputted",
+)
+@click.option(
+    "--force",
+    is_flag=True,
+    default=False,
+    help="Run git-filter-repo with --force option",
+)
 @click.argument(
-    "upstream-url", type=str, default="https://github.com/automuteus/automuteus"
+    "destination",
+    type=click.Path(file_okay=False, exists=False),
+)
+@click.argument(
+    "upstream-url",
+    type=str,
+    default="https://github.com/automuteus/automuteus",
 )
 def automuteus_update(
     input: str,
@@ -52,6 +107,15 @@ def automuteus_update(
     destination: str,
     upstream_url: str,
 ):
+    """
+    \b
+    Update automuteus repository that is filtered previously.
+
+    \b
+    Arguments:
+        destination: Path to destination(local repository) directory
+        upstream_url: URL to upstream repository
+    """
     Update(
         destination, input, output if output is not None else input, upstream_url, force
     )
@@ -60,6 +124,9 @@ def automuteus_update(
 def main():
     try:
         cli()
+    except SystemExit as e:
+        if e.code == 0:
+            return
     except:
         console.print_exception()
 
